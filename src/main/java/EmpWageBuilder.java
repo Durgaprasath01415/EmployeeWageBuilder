c16b6e5bd405eea82a8578aa9bbeb07b8d2b5a31
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-19 23:32:53 +0530

    ADDED:Funtionlity to Calculate PartTime Employee Wage

--- src/main/java/EmpWageBuilder.java
+++ src/main/java/EmpWageBuilder.java
@@ -1,18 +1,31 @@
 import javax.swing.*;
 
-public class EmpWageBuilder {
-    public static void main(String[] args) {
+public class EmpWageBuilder
+{
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Employee Wage Computation");
         int IS_FULL_TiME = 1;
+        int IS_PART_TIME = 2;
         int RatePerHour = 20;
         int FullHour = 8;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TiME) {
+        int PartHour = 4;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        System.out.println(empCheck);
+        if (empCheck == IS_FULL_TiME)
+        {
             System.out.println("Employee is Full Time Present");
             int Wage = (RatePerHour * FullHour);
-            System.out.println("Wage of Employee = " +Wage);
+            System.out.println("Wage of Employee = " + Wage);
         }
-        else {
+        else if (empCheck == IS_PART_TIME)
+        {
+            System.out.println("Employee is Part Time Present");
+            int Wage = (RatePerHour * PartHour);
+            System.out.println("Wage for Part Time= " + Wage);
+        }
+        else
+        {
             System.out.println("Employee is Absent");
             System.out.println("Wage = 0");
         }
