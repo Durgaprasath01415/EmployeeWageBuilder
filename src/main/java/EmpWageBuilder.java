c6192d7e91ead06d75ee00c5a23486759719a379
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-19 23:27:01 +0530

    ADDED:Funtionlity to calculate Employee Wages

--- src/main/java/EmpWageBuilder.java
+++ src/main/java/EmpWageBuilder.java
@@ -1,11 +1,20 @@
+import javax.swing.*;
+
 public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int IS_FULL_TiME = 1;
-        double empCheck = Math.floor(Math.random() * 10) %2;
-        if (empCheck == IS_FULL_TiME)
-            System.out.println("Employee is Present");
-        else
+        int RatePerHour = 20;
+        int FullHour = 8;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TiME) {
+            System.out.println("Employee is Full Time Present");
+            int Wage = (RatePerHour * FullHour);
+            System.out.println("Wage of Employee = " +Wage);
+        }
+        else {
             System.out.println("Employee is Absent");
+            System.out.println("Wage = 0");
+        }
     }
 }
