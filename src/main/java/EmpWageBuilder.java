cd0fbba8a1f91bf0faf300901d39703300740a54
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-20 12:35:36 +0530

    MODIFIED:To Calculate Wage Based on Month Or Working Hour

--- src/main/java/EmpWageBuilder.java
+++ src/main/java/EmpWageBuilder.java
@@ -1,18 +1,26 @@
+import com.sun.source.tree.WhileLoopTree;
+
 import javax.swing.*;
 
-public class EmpWageBuilder {
-    public static void main(String[] args) {
+public class EmpWageBuilder
+{
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Employee Wage Computation");
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
-        final int RatePerHour = 20;
-        final int No_Of_Working_Days = 20;
-        int empHrs = 0;
-        int TotalSalary = 0;
-        for (int day = 1; day <= No_Of_Working_Days; day++) {
+        final int RATEPERHOUR = 20;
+        final int NO_OF_WORKING_DAYS = 20;
+        final int MAX_HRS_IN_MONTH = 100;
+
+        int empHrs = 0 , TotalSalary = 0 , TotalEmpHrs=0 , TotalWorkingDays =0;
+        while (TotalEmpHrs <= MAX_HRS_IN_MONTH && TotalWorkingDays < NO_OF_WORKING_DAYS)
+        {
+            TotalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             System.out.println(empCheck);
-            switch (empCheck) {
+            switch (empCheck)
+            {
                 case IS_FULL_TIME:
                     System.out.println("Full Time Employee");
                     empHrs = 8;
@@ -26,7 +34,7 @@ public class EmpWageBuilder {
                     empHrs = 0;
                     break;
             }
-            int Wage = (RatePerHour * empHrs);
+            int Wage = (RATEPERHOUR * empHrs);
             System.out.println("Wage of Employee = " + Wage);
             TotalSalary += Wage;
         }
