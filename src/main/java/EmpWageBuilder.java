d0cd2c25efecb15738c0563457504667c3dbe7cb
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-24 12:40:04 +0530

    MODIFIED: To save total wage for each company

--- src/main/java/EmpWageBuilder.java
+++ src/main/java/EmpWageBuilder.java
@@ -5,10 +5,11 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    public final String Company;
-    public final int ratePerHour;
-    public final int no_of_Working_Days;
-    public final int maxHour;
+    private final int ratePerHour;
+    private final int no_of_Working_Days;
+    private final int maxHour;
+    private final String Company;
+    private int totalEmpWage;
 
     public EmpWageBuilder(String company, int ratePerHour, int no_of_Working_Days, int maxHour) {
         this.Company = company;
@@ -17,7 +18,7 @@ public class EmpWageBuilder {
         this.maxHour = maxHour;
     }
 
-    public int calculationEmpWage() {
+    public void calculationEmpWage() {
         int empHrs = 0, totalSalary = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         while (totalEmpHrs <= maxHour && totalWorkingDays < no_of_Working_Days) {
             totalWorkingDays++;
@@ -33,16 +34,23 @@ public class EmpWageBuilder {
                     empHrs = 0;
                     break;
             }
-            int Wage = (ratePerHour * empHrs);
-            totalSalary += Wage;
+            totalEmpHrs += empHrs;
+            System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
-        return totalSalary;
+        totalEmpWage = totalEmpHrs * ratePerHour;
+    }
+
+    @Override
+    public String toString(){
+        return "Total Emp Wage for Company: " +Company + " is: "+totalEmpWage;
     }
     public static void main(String[] args) {
-        EmpWageBuilder Dmart = new EmpWageBuilder ("Dmart", 20, 20, 100);
+        EmpWageBuilder dMart = new EmpWageBuilder ("Dmart", 20, 20, 100);
         EmpWageBuilder Infosys = new EmpWageBuilder("Infosys", 20, 25, 150);
-        System.out.println("Employee Wage for Dmart " + Dmart.calculationEmpWage());
-        System.out.println("Employee Wage for Infosys " + Infosys.calculationEmpWage());
+        dMart.calculationEmpWage();
+        System.out.println(dMart);
+        Infosys.calculationEmpWage();
+        System.out.println(Infosys);
         }
     }
 
