d2528c961e7f64a20f01adf0f5459b1c9c462495
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-25 17:08:57 +0530

    MODIFIED: To manage Employee wage of multiple companies

---
+++ src/main/java/CompanyEmpWage.java
@@ -0,0 +1,23 @@
+public class CompanyEmpWage {
+    public final String company;
+    public final int ratePerHour;
+    public final int numOfWorkingDays;
+    public final int maxHoursPerMonth;
+    public int totalEmpWage;
+
+    public CompanyEmpWage(String company, int ratePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.company = company;
+        this.ratePerHour = ratePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+    public void setTotalEmpWage(int totalEmpWage){
+
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    @Override
+    public String toString(){
+        return "Total Emp Wage for Company "+ company + " is: "+ totalEmpWage;
+    }
+}
