b041980291b2d6eabe205ba88567f03d21835fa9
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-05-25 20:01:59 +0530

    REFACTORED:EmpWageBuilder to implement Interface

--- src/main/java/CompanyEmpWage.java
+++ src/main/java/CompanyEmpWage.java
@@ -1,3 +1,9 @@
+
+interface IComputeEmpWage {
+    public void addCompanyEmpWage(String company,int ratePerHour, int numOfWorkingDays,int maxHoursPerMonth);
+    public void computeEmpWage();
+    public int getTotalWage(String company);
+}
 public class CompanyEmpWage {
     public final String company;
     public final int ratePerHour;
